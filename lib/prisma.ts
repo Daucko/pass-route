359778dad58d6361e594a00488ee62cccbceb92b
Daucko <dauda.a.tijani@gmail.com>
2025-12-05 14:31:35 +0000

    Fix Prisma deployment: use default client location and add postinstall script

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,7 +1,7 @@
 // lib/prisma.ts
 // Prisma client singleton to avoid multiple instances in development
 
-import { PrismaClient } from '@/app/generated-prisma-client';
+import { PrismaClient } from '@prisma/client';
 
 import { Pool } from 'pg';
 import { PrismaPg } from '@prisma/adapter-pg';
